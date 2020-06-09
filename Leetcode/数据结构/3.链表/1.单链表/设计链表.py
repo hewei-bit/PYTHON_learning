5bc4795ec98cc0b4e0237b5bab341da94b33cb5b
hewei-bit <1003826976@qq.com>
2020-06-09 10:57:51 +0800

    刷题大业

--- Leetcode/数据结构/3.链表/1.单链表/设计链表.py
+++ Leetcode/数据结构/3.链表/1.单链表/设计链表.py
@@ -1,5 +1,7 @@
+"""
+707.设计链表（Medium）
+"""
 # 自定义单节点
-
 class ListNode:
     def __init__(self, x):
         self.val = x
@@ -7,59 +9,77 @@ class ListNode:
 
 
 class MyLinkedList:
-    """
-    Initialization
-    """
 
     def __init__(self):
-        # 链表长度
-        self._lenth = 0
-        # 设置头结点
+        """
+        Initialize your data structure here.
+        """
+        # 保存链表长度
+        self._length = 0
+        # 设置哑节点
         self._head = ListNode(0)
 
     def get(self, index: int) -> int:
-        if index < 0 or index > self._lenth:
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        """
+        if index < 0 or index >= self._length:
             return -1
         cur = self._head
-        for i in range(index):
+        for _ in range(index + 1):
             cur = cur.next
         return cur.val
 
     def addAtHead(self, val: int) -> None:
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        """
+        # 转化为向索引为0的位置插入节点
         return self.addAtIndex(0, val)
 
     def addAtTail(self, val: int) -> None:
-        return self.addAtIndex(self._lenth, val)
+        """
+        Append a node of value val to the last element of the linked list.
+        """
+        # 转化为向索引为length的位置插入节点
+        return self.addAtIndex(self._length, val)
 
     def addAtIndex(self, index: int, val: int) -> None:
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        """
         if index < 0:
             index = 0
-        if index > self._lenth:
-            return None
+        if index > self._length:
+            return
         pre = self._head
         node = ListNode(val)
         for _ in range(index):
             pre = pre.next
         node.next = pre.next
         pre.next = node
-        self._lenth += 1
+        self._length += 1
 
     def deleteAtIndex(self, index: int) -> None:
-        if index < 0 or index > self._lenth:
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        """
+        if index < 0 or index >= self._length:
             return
         pre = self._head
         for _ in range(index):
             pre = pre.next
         pre.next = pre.next.next
-        self._lenth -= 1
+        self._length -= 1
 
     @property
     def lenth(self):
-        return self._lenth
+        return self._length
 
     @property
     def head(self):
         return self._head
+
 
 
 if __name__ == '__main__':
@@ -74,5 +94,5 @@ if __name__ == '__main__':
         node = node.next
     print()
     obj.deleteAtIndex(1)
-    for i in range(1, obj.lenth + 1):
+    for i in range(obj.lenth +1):
         print(obj.get(i), end=",")
