428ddcdee297e731cc12548f157c8280e436a038
hewei-bit <1003826976@qq.com>
2020-06-14 17:59:52 +0800

    刷题大业——字符串

--- Leetcode/数据结构/2.数组和字符串/1.数组/3.合并区间.py
+++ Leetcode/数据结构/2.数组和字符串/1.数组/3.合并区间.py
@@ -3,6 +3,23 @@
 """
 
 from typing import List
+
+
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return []
+        res = []
+        intervals.sort(key=lambda x: x[0])
+        for inter in intervals:
+            if res == [] or res[-1][1] < inter[0]:
+                res.append(inter)
+            else:
+                res[-1][1] = max(res[-1][1], inter[1])
+        return res
 
+
+if __name__ == '__main__':
+    ll = [[1, 6], [2, 3], [7, 9]]
+    obj = Solution().merge(ll)
+    print(obj)
