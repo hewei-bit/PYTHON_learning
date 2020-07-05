3c4b835781a2e2e9c2c51ae9815f81df180bbdb1
hewei-bit <1003826976@qq.com>
2020-07-05 22:11:27 +0800

    辅助

--- 科研/工具/collections_examlpe.py
+++ 科研/工具/collections_examlpe.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 from collections import OrderedDict
 from collections import Counter
 
+
 class collections_example:
     # amedtuple是一个函数，它用来创建一个自定义的tuple对象，
     # 并且规定了tuple元素的个数，并可以用属性而不是索引来引用tuple的某个元素
@@ -13,19 +14,22 @@ class collections_example:
         print(p.x)
         print(p.y)
         print(isinstance(p, tuple))
+
     # deque是为了高效实现插入和删除操作的双向列表，适合用于队列和栈：
     def deque_example(self):
         q = deque(['a', 'b', 'c'])
         q.append('x')
         q.appendleft('y')
         print(q)
+
     # 使用dict时，如果引用的Key不存在，就会抛出KeyError。
     # 如果希望key不存在时，返回一个默认值，就可以用defaultdict
     def defaultdict_example(self):
-        dd = defaultdict(lambda:'N/A')
+        dd = defaultdict(lambda: 'N/A')
         dd['key1'] = 'abc'
         print(dd['key1'])
         print(dd['key2'])
+
     # 使用dict时，Key是无序的。在对dict做迭代时，我们无法确定Key的顺序。
     # 如果要保持Key的顺序，可以用OrderedDict：
     def OrderedDict_example(self):
@@ -44,7 +48,7 @@ class collections_example:
     def counter_helpe(self):
         c = Counter()
         for ch in 'programming':
-            c[ch] = c[ch]+1
+            c[ch] = c[ch] + 1
         print(c['r'])
         print(c)
 
